85cd4ea216291eb53011d7bf9756ed7eca34310b
Ben Spencer <benspencer39@gmail.com>
2017-05-17 10:19:39 +0100

    Add empty document ready and window load / resize

---
+++ js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function($) {
+
+  // On document ready...
+  $(document).ready(function() {
+
+  });
+
+
+  // On window load...
+  $(window).load(function() {
+
+  });
+
+
+  // On window resize...
+  $(window).resize(function() {
+
+  });
+
+})(jQuery);
